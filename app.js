365af9682d5b024a8ab04d38ec985294b94e14ca
Ian <egill.ian1@gmail.com>
2020-09-09 22:03:01 -0400

    Basic system for registering

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const createError = require('http-errors');
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const expressEjsLayout = require('express-ejs-layouts');
+const sassMiddleware = require('node-sass-middleware');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// mongoose
+mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
+.then(() => console.log('connected to mongodb'))
+.catch((err)=> console.log(err));
+// EJS
+app.set('view engine','ejs');
+app.use(expressEjsLayout);
+// BodyParser
+app.use(express.urlencoded({extended : false}));
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,9 +34,6 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,5 +50,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//app.listen(port);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,89 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Login handle
+router.get('/login',(req,res)=>{
+    res.render('login');
+})
 
-module.exports = router;
+router.get('/register',(req,res)=>{
+    res.render('register')
+})
+
+// Register handle
+router.post('/register',(req,res)=>{
+    const {email, password, password2} = req.body;
+    let errors = [];
+    console.log('Register for name email :' + email + ' pass:' + password);
+    if(!email || !password || !password2) {
+        errors.push({msg : "Please fill in all fields"})
+    }
+
+    // Check if match
+    if(password !== password2) {
+        errors.push({msg : "Passwords dont match"});
+    }
+
+    // Check if password is more than 6 characters
+    if(password.length < 6 ) {
+        errors.push({msg : 'Password must be at least 6 characters'})
+    }
+
+    // Send error messages to user
+    if(errors.length > 0 ) {
+        console.log("Errors found: ", errors);
+        res.render('register', {
+            errors : errors,
+            email : email,
+            password : password,
+            password2 : password2})
+        return
+    }
+
+    // Find duplicate
+    User.findOne({email : email}).exec((err,user)=>{
+        if(user) {
+            errors.push({msg: 'Email already registered'});
+            render(res, errors, email, password, password2);
+            return
+        }
+    });
+
+    // Validation passed, create user
+
+    // Hash password
+    bcrypt.genSalt(10,(err, salt) =>
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log("hashing parameters", password, salt, hash);
+        if(err) {
+            throw err;
+        }
+
+        // Create and save user
+        const newUser = new User({
+           email: email,
+           password: hash
+        });
+
+        console.log("newUser", newUser);
+
+        newUser.save().then((value) => {
+           console.log(value)
+           res.redirect('/users/login');
+        })
+        .catch(value=> console.log(value));
+    }));
+})
+
+router.post('/login',(req,res,next)=>{
+
+})
+
+//logout
+router.get('/logout',(req,res)=>{
+
+})
+
+module.exports  = router;
